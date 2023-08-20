ad9a8d736eaef9e7a3595fb1d9386dd006dfea41
AkashItlogiko <139780035+AkashItlogiko@users.noreply.github.com>
2023-08-20 21:43:10 +0600

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php 
+
+// $animal = 'cat';
+// $Animal = 'cat';
+//  echo 'i love ' .$Animal ."<br>";
+//  /*khalahobe
+//  khalaHobe
+//  Khala_hobe*/
+
+//  $x=100;
+//  $y=10;
+
+// // $_age=10;
+// // $age7=10;
+// // $A&GE=10;
+
+// echo $x + $y .'<br>';
+// echo $x+  $y .'<br>';
+// echo ($x+$y) .'<br>';
+
+// print $animal ;
+// $animal = print('dog');
+// echo $animal;
+
+// $name ='Jone';
+// $name1 ="it's me";
+// $age= 30;
+// $age1=30.4;
+// $religion = true;
+// $people = array('Orange','apple','Banana');
+// $null= null;
+// var_dump($null);
+
+// class phone{
+//   var $model;
+//   function phonemodel ($number){
+//     global $model;
+//     $model = $number;
+//     echo"This is $model <br>";
+//   }
+// }
+// $apple = new phone;
+// $apple-> phoneModel('iphone 13');
+
+
+// $nokia = new phone;
+// $nokia ->phonemodel('1210');
+// $lg = new phone;
+// $lg ->phonemodel('upcoming'); -->
+
+//  echo str_replace ('flower','Dog','i love flower',);
+// $x=5;
+// $y=10;
+// var_dump($x);
+
+// echo(sqrt(34));
+// echo(round(pi()));
+// echo(rand(1,6));
+//  $x=5;
+//  $y=10;
+//  $y=18;
+
+// define('a',100);
+// define('A',200);
+// define('cars',['BMW','Toyata','Mazda']);
+//  echo(cars[2]);
+//  var_dump(cars);
+
+//  function name(){
+//  $x=100;
+//  echo($x);
+//  echo(cars[2]);
+//  }
+//  name();
+
+// $x = 2;
+// $y = 5;
+// For Exponentiation
+// echo $x ** $y;
+// PHP Assignment Operators
+// $x = 2;
+// $y = 12;
+
+// $x=$x+$y;//$z =$x+$y;
+// $x=$x*$y;
+// //echo $x = $y;
+// echo $x;
+//Comparison operators
+// $x= 500;
+// $y= 50;
+// var_dump($x>$y);
+//if & else Statements
+// $date = date('H');
+// if(22 < 20){
+// echo 'Good Morning'.$date;
+// }elseif(22>25){
+//   echo 'Good evening ';
+// }
+//  else{
+//   echo 'Have a nice day';
+// }
+// Php switch Statement
+// switch('apple'){
+//  case 'red';
+//  echo 'Your pen is red';
+//  break;
+//  case 'food';
+//  echo 'I love food';
+//  break;
+//  default;
+//  echo 'i love all food';
+// }
+// Php for loop
+// $x = 2;
+// while($x <=5){
+// echo "The number is :$x <br>";
+// $x++;
+// }
+// $x=1;
+// do{
+// echo "The number is:$x <br>";
+// $x++;  
+// }while($x <=5);
+
+// for($x=0;$x<=5;$x++){
+// echo "The number is:$x <br>"; 
+// }
+// Php Functions
+// function test_name($name,$age=2){
+// echo"This is function $name and his age is $age <br>";
+// }
+// test_name('shuvo',25);
+// test_name('Ali',26);
+// test_name('Joy');
+
+// Php date and time
+date_default_timezone_set("Asia/Dhaka");
+echo "Today is <br>".date("d/m/Y")."<br>".date("h:i:sa") ."<br>";
+?>
+
+</body>
+</html>
